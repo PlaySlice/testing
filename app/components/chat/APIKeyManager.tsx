e875b24fe6be04d5c952f3e729d247cc90fd0834
PlaySlice <mikem8622@icloud.com>
2025-03-08 23:22:46 +0200

    testing api removal

--- app/components/chat/APIKeyManager.tsx
+++ app/components/chat/APIKeyManager.tsx
@@ -117,15 +117,15 @@ export const APIKeyManager: React.FC<APIKeyManagerProps> = ({ provider, apiKey, 
         {isEditing ? (
           <div className="flex items-center gap-2">
             <input
-              type="password"
+/*               type="password"
               value={tempKey}
               placeholder="Enter API Key"
               onChange={(e) => setTempKey(e.target.value)}
               className="w-[300px] px-3 py-1.5 text-sm rounded border border-bolt-elements-borderColor 
                         bg-bolt-elements-prompt-background text-bolt-elements-textPrimary 
                         focus:outline-none focus:ring-2 focus:ring-bolt-elements-focus"
-            />
-            <IconButton
+            /> */
+{/*             <IconButton
               onClick={handleSave}
               title="Save API Key"
               className="bg-green-500/10 hover:bg-green-500/20 text-green-500"
@@ -138,28 +138,28 @@ export const APIKeyManager: React.FC<APIKeyManagerProps> = ({ provider, apiKey, 
               className="bg-red-500/10 hover:bg-red-500/20 text-red-500"
             >
               <div className="i-ph:x w-4 h-4" />
-            </IconButton>
+            </IconButton> */}
           </div>
         ) : (
           <>
             {
-              <IconButton
+/*               <IconButton
                 onClick={() => setIsEditing(true)}
                 title="Edit API Key"
                 className="bg-blue-500/10 hover:bg-blue-500/20 text-blue-500"
               >
                 <div className="i-ph:pencil-simple w-4 h-4" />
-              </IconButton>
+              </IconButton> */
             }
             {provider?.getApiKeyLink && !apiKey && (
-              <IconButton
+ /*              <IconButton
                 onClick={() => window.open(provider?.getApiKeyLink)}
                 title="Get API Key"
                 className="bg-purple-500/10 hover:bg-purple-500/20 text-purple-500 flex items-center gap-2"
               >
                 <span className="text-xs whitespace-nowrap">{provider?.labelForGetApiKey || 'Get API Key'}</span>
                 <div className={`${provider?.icon || 'i-ph:key'} w-4 h-4`} />
-              </IconButton>
+              </IconButton> */
             )}
           </>
         )}
